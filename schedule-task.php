791836ae5e768239045b366a68072225f0932b27
arifmanzoor11 <arifmanzoor11@gmail.com>
2025-04-28 14:17:19 +0530

    impove auto_import_interval as it was firing each 2 minutes

--- schedule-task.php
+++ schedule-task.php
@@ -2,7 +2,6 @@
 // Define the custom intervals based on database value
 function my_custom_cron_intervals($schedules) {
     // Get the interval from database
-    $import_interval = get_option('auto_import_interval', '14400'); // Default to 4 hours if not set
     
     // Create an array of allowed intervals
     $allowed_intervals = array(
@@ -14,6 +13,9 @@ function my_custom_cron_intervals($schedules) {
         '86400' => 'Daily',
         '604800' => 'Weekly'
     );
+    
+    $import_interval = get_option('auto_import_interval', '14400'); // Default to 4 hours if not set
+    $import_interval = in_array($import_interval, array_keys($allowed_intervals)) ? $import_interval : '14400';
 
     // Add the custom interval from database
     $schedules['custom_interval'] = array(
@@ -25,26 +27,39 @@ function my_custom_cron_intervals($schedules) {
 }
 add_filter('cron_schedules', 'my_custom_cron_intervals');
 
+
+
 // Function to be called by the cron job
 function my_cron_job_function() {
+    error_log('Cron executed at: ' . current_time('mysql'));
     download_and_import_properties_file();
 }
 add_action('my_cron_hook', 'my_cron_job_function');
 
+
+
 // Schedule an event if it's not already scheduled
 function my_cron_schedule_activation() {
-    // Clear existing schedule
+    // First, clear any existing schedules
     $timestamp = wp_next_scheduled('my_cron_hook');
     if ($timestamp) {
         wp_unschedule_event($timestamp, 'my_cron_hook');
     }
-
-    // Schedule new event with custom interval
+    
+    // Check if custom_interval exists
+    $schedules = apply_filters('cron_schedules', []);
+    if (!isset($schedules['custom_interval'])) {
+        error_log('Custom interval NOT registered!');
+    } else {
+        error_log('Custom interval is set to: ' . $schedules['custom_interval']['interval']);
+    }
+    
+    // Only schedule if not already scheduled
     if (!wp_next_scheduled('my_cron_hook')) {
         wp_schedule_event(time(), 'custom_interval', 'my_cron_hook');
     }
 }
-add_action('wp', 'my_cron_schedule_activation');
+register_activation_hook(__FILE__, 'my_cron_schedule_activation');
 
 // Clear scheduled event upon plugin deactivation
 function my_cron_schedule_deactivation() {
